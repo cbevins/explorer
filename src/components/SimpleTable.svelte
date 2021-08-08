0d7958a76bd95c4fca852f671382195b2c4f8e06
Collin D. Bevins <cbevins@montana.com>
2021-08-08 16:05:54 -0600

    Made collapsible

--- src/components/SimpleTable.svelte
+++ src/components/SimpleTable.svelte
@@ -1,22 +1,30 @@
 <script>
   export let data // array of rows, each row an array of cols
+  export let id
   export let title
+  let target = '#' + id
 </script>
 
 <div class='card'>
   <div class="card-body">
-  {#if title}
-    <h5 class="card-title">{title}</h5>
-    <hr/>
-  {/if}
-  <table class='table table-striped'>
-    {#each data as row}
-    <tr>
-      {#each row as col}
-        <td>{col}</td>
-      {/each}
-    </tr>
-    {/each}
-  </table>
+    <h5 class="card-title">{title}
+     <button class="btn btn-primary" type="button" data-bs-toggle="collapse" aria-expanded="false"
+      data-bs-target={target} aria-controls={id}>
+        +/-
+     </button>
+    </h5>
+
+
+    <div class="collapse show" id={id}>
+      <table class='table table-striped'>
+        {#each data as row}
+          <tr>
+            {#each row as col}
+              <td>{col}</td>
+            {/each}
+          </tr>
+        {/each}
+      </table>
+    </div>
   </div>
 </div>
