335396577a1832cb06219538d2fff833a9561630
Collin D. Bevins <cbevins@montana.com>
2021-08-14 15:30:56 -0600

    Removed FireMeshArray

--- src/models/FireMesh/FireMesh.js
+++ src/models/FireMesh/FireMesh.js
@@ -1,4 +1,4 @@
-import { FireMeshArray } from './FireMeshArray.js'
+import { FireMeshLine } from './FireMeshLine.js'
 import { Period } from '../GeoFire/Period.js'
 
 export class FireMesh {
@@ -12,29 +12,75 @@ export class FireMesh {
     this._bounds = bounds
     this._fireBehaviorProvider = fireBehaviorProvider
     this._fireInputProvider = fireInputProvider
-    this._horz = new FireMeshArray(bounds, true)
+    this._horz = []
+    for (let y = bounds.north(); y >= bounds.south(); y -= bounds.ySpacing()) {
+      this._horz.push(new FireMeshLine(y))
+    }
     this._period = new Period()
-    this._vert = new FireMeshArray(bounds, false)
+    this._vert = []
+    for (let x = bounds.west(); x <= bounds.east(); x += bounds.xSpacing()) {
+      this._vert.push(new FireMeshLine(x))
+    }
   }
 
+  // ---------------------------------------------------------------------------
+  // Public property accessors
+  // ---------------------------------------------------------------------------
+
   bounds () { return this._bounds }
-  horz () { return this._horz }
+
+  horzArray () { return this._horz }
+
+  // Returns index of horizontal FireMeshLine closest to *y*
+  horzIdxAt (y) {
+    const pt = this.bounds().snapY(y)
+    return Math.max(0, Math.min(this.bounds().yInterval(pt), this._horz.length - 1))
+  }
+
+  // Returns reference to the horizontal FireMeshLine at array offset *idx*
+  horzLine (idx) { return this._horz[idx] }
+
+  // Returns reference to horizontal FireMeshLine most closely anchored to *y*
+  horzLineAt (y) { return this._horz[this.horzIdxAt(y)] }
+
   period () { return this._period }
-  vert () { return this._vert }
+
+  vertArray () { return this._vert }
+
+  // Returns index of vertical line closest to *x*
+  vertIdxAt (x) {
+    const xpt = this.bounds().snapX(x)
+    return Math.min(Math.max(0, this.bounds().xInterval(xpt)), this._vert.length - 1)
+  }
+
+  // Returns reference to the vertical FireMeshLine at array offset *idx*
+  vertLine (idx) { return this._vert[idx] }
+
+  // Returns reference to vertical FireMeshLine most closely anchored to *x*
+  vertLineAt (x) { return this._vert[this.vertIdxAt(x)] }
+
+  // ---------------------------------------------------------------------------
+  // Public mutator and processor methods
+  // ---------------------------------------------------------------------------
 
   burnForPeriod (duration) {
     // Update the current burning period
     this.period().update(duration)
 
     // Expand the endpoints of every Burned FireMeshSegment
-    this._horz.lines().forEach((line, lidx) => {
+    this._horz.forEach((line, lidx) => {
       const y = line.anchor()
       line.segments().forEach((segment, sidx) => {
         if (segment.isBurned()) {
           const input0 = this._fireInputProvider.get(segment.begins(), y, this.period().begins(), duration)
+          const fire0 = this._fireInputProvider.getFireBehavior(input0)
           const input1 = this._fireInputProvider.get(segment.ends(), y, this.period().begins(), duration)
+          const fire1 = this._fireInputProvider.getFireBehavior(input1)
         }
       })
     })
+  }
+
+  igniteAt (x, y) {
   }
 }
--- src/models/FireMesh/FireMesh.test.js
+++ src/models/FireMesh/FireMesh.test.js
@@ -2,26 +2,57 @@
 import { expect, test } from '@jest/globals'
 import { GeoBounds } from '../Geo/index.js'
 import { FireMesh } from './FireMesh.js'
-import { FireBehaviorProvider, FireInputProviderMock } from '../FireBehavior/index.js'
+import { FireMeshBehaviorProvider } from './FireMeshBehaviorProvider.js'
+import { FireMeshInputProviderMock } from './FireMeshInputProviderMock.js'
 
 const west = 1000
 const east = 2000
 const north = 5000
-const south = 4000
+const south = 3000
 const spacing = 1
 const bounds = new GeoBounds(west, north, east, south, spacing, spacing)
 
-test('1: FireMesh()', () => {
-  const fireInputProvider = new FireInputProviderMock()
-  const fireBehaviorProvider = new FireBehaviorProvider()
+const fireInputProvider = new FireMeshInputProviderMock()
+const fireBehaviorProvider = new FireMeshBehaviorProvider()
+
+test('1: FireMesh constructor', () => {
   const mesh = new FireMesh(bounds, fireInputProvider, fireBehaviorProvider)
   expect(mesh.bounds()).toEqual(bounds)
+  expect(bounds.rows()).toEqual(2001)
+  expect(bounds.cols()).toEqual(1001)
+  expect(mesh.horzArray().length).toEqual(2001)
+  expect(mesh.vertArray().length).toEqual(1001)
+})
 
-  expect(mesh.horz().lines().length).toEqual(bounds.cols())
-  expect(mesh.horz().isHorizontal()).toEqual(true)
-  expect(mesh.horz().isVertical()).toEqual(false)
+test('2: GeoBounds snapX(), snapY(), xInterval(), yInterval()', () => {
+  expect(bounds.snapX(1500.4)).toEqual(1500)
+  expect(bounds.snapX(1500.5)).toEqual(1501)
+  expect(bounds.snapY(4500.95)).toEqual(4501)
+  expect(bounds.xInterval(bounds.west())).toEqual(0)
+  expect(bounds.xInterval(1500)).toEqual(500)
+  expect(bounds.xInterval(bounds.east())).toEqual(1000)
+  expect(bounds.yInterval(bounds.north())).toEqual(0)
+  expect(bounds.yInterval(4750)).toEqual(250)
+  expect(bounds.yInterval(4250)).toEqual(750)
+  expect(bounds.yInterval(bounds.south())).toEqual(2000)
+})
 
-  expect(mesh.vert().lines().length).toEqual(bounds.rows())
-  expect(mesh.vert().isHorizontal()).toEqual(false)
-  expect(mesh.vert().isVertical()).toEqual(true)
+test('3: FireMesh horzIdxAt(), vertIdxAt()', () => {
+  const mesh = new FireMesh(bounds, fireInputProvider, fireBehaviorProvider)
+  expect(mesh.horzIdxAt(bounds.north())).toEqual(0)
+  expect(mesh.horzIdxAt(bounds.south())).toEqual(2000)
+  expect(mesh.horzIdxAt(bounds.north() + 100)).toEqual(0) // out-of-bounds
+  expect(mesh.horzIdxAt(bounds.south() - 100)).toEqual(2000) // out-of-bounds
+
+  expect(mesh.vertIdxAt(bounds.west())).toEqual(0)
+  expect(mesh.vertIdxAt(bounds.east())).toEqual(1000)
+  expect(mesh.vertIdxAt(bounds.west() - 100)).toEqual(0)
+  expect(mesh.vertIdxAt(bounds.east() + 100)).toEqual(1000)
+
+  const idx = mesh.horzIdxAt(bounds.north())
+  expect(idx).toEqual(0)
+  expect(mesh.horzLine(idx).anchor()).toEqual(5000)
+
+  const hline = mesh.horzLineAt(4000)
+  expect(hline.anchor()).toEqual(4000)
 })
