bba24e23a7eec39784989f8188289f9b50451281
Collin D. Bevins <cbevins@montana.com>
2021-08-15 17:43:48 -0600

    Refactored for further use

--- src/routes/sprite.svelte
+++ src/routes/sprite.svelte
@@ -2,13 +2,26 @@
 	import { onMount } from 'svelte'
   import { FireEllipse } from './../models/FireEllipse'
   import { FireFly, vectorPath } from '../models/FireSprites/FireFly.js'
+  import SimpleTable from '../components/SimpleTable.svelte'
 
   // Parameters
   const width = 200
   const height = 200
   const nsprites = 15
   const r = 1 // FireFly radius
-	let canvas, ctx, frame
+  const maxSteps = 1000
+
+  // Must be reset()
+  let cellsTraversed = 0
+  let stepNumber = 0
+  let running = true
+  let head, sprites // FireFly swarm
+  let burnSummary = []
+
+  // These properties are set by onMount():
+	let canvas // reference to the on-screen HTML <canvas> element
+  let ctx // reference to the canvas 2d context
+  let frame // id of the current animation frame
 
   // Field fire codes
   const Unburnable = 0
@@ -18,14 +31,23 @@
   const Color = ['gray', 'green', '#ff8c00', '#cd853f'] // gray, green, darkorange, peru
   const field = new Array(width * height).fill(Unburned)
 
-  // To convert between compass and device degrees, in either direction:
+  // For now, use a single fire behavior ellipse for the entire landscape
+  let fire = new FireEllipse(100, 50, 135, 1)
+  const fireSummary = [
+    ['Head Ros', fire.headRate().toFixed(2)],
+    ['Fire Heading', fire.headDegrees().toFixed(0)],
+    ['Head at', `[${fire.hx().toFixed(2)}, ${fire.hy().toFixed(2)}]`],
+    ['Back at', `[${fire.bx().toFixed(2)}, ${fire.by().toFixed(2)}]`]
+  ]
+
+  // Position the ignition point at an optimal location in the landscape
+  const ignX = 2 * Math.ceil(width * Math.abs((fire.bx() / fire.hx())))
+  const ignY = height - 2 * Math.ceil(height * Math.abs((fire.bx() / fire.hx())))
+
+  // Converts between compass degrees (clockwise wrt North)
+  // and device degrees (counter-clockwise wrt East), in either direction:
   function rotate (deg) { return (450 - deg) % 360 }
   //;[0, 45, 90, 135, 180, 225, 270, 315, 360].forEach(deg => {console.log(`${deg} <-> ${rotate(deg)}`)})
-
-  // Get the current fire behavior
-  let fire = new FireEllipse(100, 50, 135, 1)
-  console.log('RoS', fire.headRate(), 'Towards', fire.headDegrees(),
-    'Head at', fire.hx(), fire.hy(), 'Back at',  fire.bx(), fire.by())
 
   // Creates a new FireFly at [x, y] heading at azimuth (degrees clockwise from north)
   function newFireFly (x, y, azimuth) {
@@ -33,65 +55,40 @@
     return new FireFly(x, y, rotate(azimuth + fire.headDegrees()), ros/fire.length())
   }
 
-  // Position the ignition point in an optimal place
-  const ignX = 2 * Math.ceil(width * Math.abs((fire.bx() / fire.hx())))
-  const ignY = height - 2 * Math.ceil(height * Math.abs((fire.bx() / fire.hx())))
-
   // Create a starter swarm of 360 FireFlys from the ignition point
-  const head = newFireFly(ignX, ignY, fire.headDegrees())
-  console.log(head)
-  let sprites = []
-  sprites.push(head)
-  for (let i=0; i<nsprites; i++) {
-    const deg = i * 360 / nsprites // degrees from fire head!
-    sprites.push(newFireFly(ignX, ignY, deg))
+  function createSwarm () {
+    head = newFireFly(ignX, ignY, fire.headDegrees())
+    sprites = []
+    sprites.push(head)
+    for (let i=0; i<nsprites; i++) {
+      const deg = i * 360 / nsprites // degrees from fire head!
+      sprites.push(newFireFly(ignX, ignY, deg))
+    }
+    sprites.push(newFireFly(100, 100, 270))
   }
-  sprites.push(newFireFly(100, 100, 270))
 
   // ---------------------------------------------------------------------------
   // Animation loop
   // ---------------------------------------------------------------------------
-  let step = 0 // loop number
-  let ncells = 0 // number of cells traverse
 
-  function loop(t) {
-    frame = requestAnimationFrame(loop)
-    step++
+  function burn() {
+    drawField()
+    drawIgnitionPoint()
     const spawn = []
-
-    ctx.fillStyle = Color[Unburned]
-    ctx.fillRect(0, 0, width, height)
-    // Field
-    for (let row=0; row< height; row++) {
-      for (let col = 0; col< width; col++) {
-        const idx = col + row * width
-        if (field[idx] !== Unburned) {
-          ctx.fillStyle = Color[field[idx]]
-          ctx.fillRect(col, height - row, 1, 1)
-        }
-      }
-    }
-    // ignition point
-    ctx.beginPath()
-    ctx.fillStyle = 'yellow'
-    ctx.ellipse(ignX, height - ignY, r, r, 0, 0, 2 * Math.PI)
-    ctx.fill()
-
     // Advance each FireFly
-    let _x0, _y0, _x1, _y1, cells
+    let cells
     let spacing = 1
     sprites.forEach((sprite, idx) => {
       ctx.beginPath()
       cells = sprite.nextPath(spacing)
       sprite.move()
 
-      // Mark each cell its travels thru as Burned
-      ncells += cells.length
+      // Mark each cell it traverses as Burned
+      cellsTraversed += cells.length
       if (!idx) { // only for head sprite for now
         cells.forEach(cell => {
           const deg = (sprite.deg + 90) % 360
           const ffly = newFireFly(cell.x, cell.y, deg)
-          console.log(`From ${sprite.x}, ${sprite.y}, Added FireFly at [${cell.x}, ${cell.y}]`)
           ffly.spawn = true
           spawn.push(ffly)
         })
@@ -102,39 +99,123 @@
 
       // Did we hit the edge of the world?
       if (sprite.x >= width-2*r || sprite.x < 2*r || sprite.y >= height-2*r || sprite.y < 2*r) {
-        console.log(`Stopped after ${step} steps and ${ncells} cells burned`)
+        // console.log(`Stopped after ${stepNumber} steps and ${cellsTraversed} cells burned`)
         cancelAnimationFrame(frame)
+        running = !running
         return
       }
-
-      // if (idx===0) console.log(`Step ${step} Sprite ${idx} [${sprite.x}, ${sprite.y}`)
-      // While sprite y-axis increases to the north, the canvas y increases to south
-      const canvasY = height - sprite.y
-      const canvasX = sprite.x
-      ctx.fillStyle = sprite.spawn ? 'yellow' : 'red'
-      ctx.ellipse(canvasX, canvasY, r, r, 0, 0, 2 * Math.PI)
-      ctx.fill()
+      drawFireFly(sprite)
     }) // sprites.forEach()
     sprites = sprites.concat(spawn)
-    // console.log(`Step ${step} has ${sprites.length} FireFlys (added ${spawn.length})`)
+
+    // Summary update
+    burnSummary = [
+      ['Step Number', stepNumber],
+      ['FireFlys Active', sprites.length],
+      ['FireFlys Added', spawn.length],
+      ['Cells Traversed', cellsTraversed]
+    ]
+  }
+
+  function drawField () {
+    ctx.fillStyle = Color[Unburned]
+    ctx.fillRect(0, 0, width, height)
+    for (let row=0; row< height; row++) {
+      for (let col = 0; col< width; col++) {
+        const idx = col + row * width
+        if (field[idx] !== Unburned) {
+          ctx.fillStyle = Color[field[idx]]
+          ctx.fillRect(col, height - row, 1, 1)
+        }
+      }
+    }
+  }
+
+  function drawFireFly (fireFly, radius = 1) {
+    // While sprite y-axis increases to the north, the canvas y increases to south
+    ctx.fillStyle = fireFly.spawn ? 'yellow' : 'red'
+    ctx.ellipse(fireFly.x, (height - fireFly.y), radius, radius, 0, 0, 2 * Math.PI)
+    ctx.fill()
+  }
+
+  function drawIgnitionPoint() {
+    ctx.beginPath()
+    ctx.fillStyle = 'yellow'
+    ctx.ellipse(ignX, height - ignY, r, r, 0, 0, 2 * Math.PI)
+    ctx.fill()
+  }
+
+  function loop(t) {
+    frame = requestAnimationFrame(loop)
+    stepNumber++
+    if (stepNumber >= maxSteps) cancelAnimationFrame(frame)
+    burn()
   }
 
 	onMount(() => {
 		ctx = canvas.getContext('2d')
-		frame = requestAnimationFrame(loop)
-		return () => {
-			cancelAnimationFrame(frame)
-		}
+    doReset()
 	})
+
+  function doReset () {
+    cellsTraversed = 0
+    stepNumber = 0
+    running = true
+    createSwarm()
+    field.fill(Unburned)
+    drawField()
+    drawIgnitionPoint()
+  }
+
+  // 'Run/Pause' button callback: starts, pauses, and resumes simulation
+  function doRun () {
+    running = !running
+    if (running) {
+      cancelAnimationFrame(frame)
+    } else {
+      frame = requestAnimationFrame(loop)
+		  // return () => { cancelAnimationFrame(frame) }
+    }
+  }
+
+  // 'Step' button callback: pauses animation and runs 1 burning period
+  function doStep () {
+    cancelAnimationFrame(frame)
+    burn()
+  }
 </script>
 
 <svelte:head>
 	<title>Tinker Project</title>
 </svelte:head>
-<p>Step {step}</p>
-<canvas
-	bind:this={canvas} width={width}	height={height}
-></canvas>
+
+<div class="row">
+  <div class="col">
+    <!-- Button row -->
+    <div class="row">
+      <div class="col">
+        <button class='btn-primary mb-3' on:click={doStep}>Step</button>
+      </div>
+      <div class="col">
+        <button class='btn-primary mb-3' on:click={doRun}>{running?'Run':'Pause'}</button>
+      </div>
+      <div class="col">
+        <button class='btn-primary mb-3' on:click={doReset}>Reset</button>
+      </div>
+    </div>
+    <!-- Burn status table -->
+    <div class="row">
+      <SimpleTable title='Burn Status' data={burnSummary} id='spriteBurnSummary' />
+    </div>
+    <div class="row">
+      <SimpleTable title='Fire Ellipse' data={fireSummary} id='spriteFireSummary' />
+    </div>
+  </div>
+
+  <div class="col-8">
+    <canvas bind:this={canvas} width={width} height={height}></canvas>
+  </div>
+</div>
 
 <style>
 	canvas {
